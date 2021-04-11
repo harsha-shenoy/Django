3dae5b015c0400b349fea5e5fb06b80e8631452f
Harsha <harshashenoy63@gmail.com>
2021-04-11 19:22:05 +0530

    Email

--- django_project/settings.py
+++ django_project/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv   #for python-dotenv method
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -130,3 +132,12 @@ LOGIN_URL = 'login'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'harshashenoy63@gmail.com'
+EMAIL_HOST_PASSWORD = 'dvxcznngkcuizzmz'
+
+
